3570f7e41091b66bf2732c60aff3d3789982d472
Parker Freestone <parkerfreestone@gmail.com>
2022-07-15 11:36:31 -0600

    added a default route

--- src/components/NavigationController.jsx
+++ src/components/NavigationController.jsx
@@ -1,48 +1,68 @@
 import React from "react";
 import { Nav, Navbar, Container } from "react-bootstrap";
 import { BoomboxFill } from "react-bootstrap-icons";
-import { Routes, Link, Route } from "react-router-dom";
+import { Routes, Link, Route, Navigate } from "react-router-dom";
 
 // ALL OUR ROUTES
 import TeamRandomizer from "../routes/TeamRandomizer";
 import StatsTracker from "../routes/StatsTracker";
+import Home from "../routes/Home";
 
 const NavigationController = () => {
     return (
         <>
             <Navbar
+                expand="lg"
                 bg="primary"
                 variant="dark"
             >
                 <Container>
-                    <Navbar.Brand
-                        className="d-flex align-items-center"
-                        href="#home"
-                    >
-                        <BoomboxFill className="mx-2" />
-                        Custom Game Tracker
+                    <Navbar.Brand>
+                        <Link
+                            to="/"
+                            className="navbar-brand"
+                        >
+                            <BoomboxFill className="mx-2" />
+                            Custom Game Tracker
+                        </Link>
                     </Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link>
-                            <Link
-                                to="/team-randomizer"
-                                className="nav-link"
-                            >
-                                Team Randomizer
-                            </Link>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <Link
-                                to="/stats-tracker"
-                                className="nav-link"
-                            >
-                                Stats Tracker
-                            </Link>
-                        </Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link>
+                                <Link
+                                    to="/"
+                                    className="nav-link"
+                                >
+                                    Home
+                                </Link>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <Link
+                                    to="/team-randomizer"
+                                    className="nav-link"
+                                >
+                                    Team Randomizer
+                                </Link>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <Link
+                                    to="/stats-tracker"
+                                    className="nav-link"
+                                >
+                                    Stats Tracker
+                                </Link>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
+
             <Routes>
+                <Route
+                    exact path="/"
+                    element={<Home />}
+                />
                 <Route
                     path="/team-randomizer"
                     element={<TeamRandomizer />}
@@ -50,6 +70,10 @@ const NavigationController = () => {
                 <Route
                     path="/stats-tracker"
                     element={<StatsTracker />}
+                />
+                <Route
+                    path="*"
+                    element={<Navigate to="/" repalce />}
                 />
             </Routes>
         </>
---
+++ src/routes/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Home = () => {
+    return (
+        <Container
+            className="mt-5"
+        >
+            <h1>Home Page</h1>
+        </Container>
+    );
+}
+
+export default Home;
